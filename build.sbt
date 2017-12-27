3246b3d731dd2628846ea937b3ae79c7cec399cd
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-28 00:40:56 +0100

    hello

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.4"
 
 libraryDependencies += "org.apache.kafka" % "kafka-streams" % "1.0.0"
-libraryDependencies += "net.manub" %% "scalatest-embedded-kafka" % "1.0.0" % "test"
-libraryDependencies += "net.manub" %% "scalatest-embedded-kafka-streams" % "0.10.0"
-
+//libraryDependencies += "net.manub" %% "scalatest-embedded-kafka" % "1.0.0" % "test"
+//libraryDependencies += "net.manub" %% "scalatest-embedded-kafka-streams" % "0.10.0"
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.25"
+libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.25"
--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,35 +1,55 @@
 package lazyevaluators.streams
 
+import collection.JavaConverters._
 import java.util.Properties
 
+import org.apache.kafka.common.internals.Topic
 import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams._
+import org.apache.kafka.streams.kstream._
+import org.apache.kafka.streams.kstream.KStream
+
 
 object Demo {
 
-  def main(args: Array[String]): Unit = {
-    val config = {
-      val properties = new Properties()
-      properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-demo")
-      properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-      properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-      properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-      properties
-    }
+  val config = {
+    val properties = new Properties()
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-demo")
+    properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    properties
+  }
 
+  def tokenize(stream: KStream[String, String]) = stream
+    .flatMapValues[String](_.split(" ").toList.asJava)
+    .map[String, String]((_, v) => new KeyValue[String, String](v, v))
+
+  def countWords(stream: KStream[String, String]): KTable[String, String] =
+    stream
+      .flatMap[String, String]((_: String, value: String) =>
+      value
+        .split(" ")
+        .map(word => new KeyValue[String, String](word, word))
+        .toList.asJava
+    ).groupByKey().count().mapValues(_.toString)
+
+  def processStream(source: String, sink: String, proc: KStream[String, String] => KStream[String, String]): Unit = {
 
     val builder = new StreamsBuilder()
-    val sourceStream = builder.stream[String, String]("demo-source")
+    val sourceStream: KStream[String, String] = builder.stream[String, String](source)
 
-    import collection.JavaConverters._
-
-    sourceStream
-      .flatMapValues[String](_.split(" ").toList.asJava)
-      .map[String, String]((_, v) => new KeyValue[String, String](v, v))
-//        .groupByKey().count().toStream
-      .to("demo-sink")
+    proc(sourceStream).to(sink)
 
     val streams = new KafkaStreams(builder.build(), config)
     streams.start()
   }
+
+  def main(args: Array[String]): Unit = {
+//    processStream("demo-source", "demo-sink", tokenize(_))
+    processStream("demo-source", "demo-sink-table", stream => countWords(stream).toStream.map[String, String](
+      (k, v) => new KeyValue[String, String](k, s"$k: $v")
+    ))
+  }
+
 }
